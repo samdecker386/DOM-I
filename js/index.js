fe81df1d8a87c41db38c000e78c4ec6f3f42788b
Sam Decker <samdecker386@gmail.com>
2020-02-10 11:56:20 -0700

    Nav bar

--- js/index.js
+++ js/index.js
@@ -40,3 +40,11 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const links = document.querySelectorAll("a");
+links[0].textContent = "Services";
+links[1].textContent = "Product";
+links[2].textContent = "Vision";
+links[3].textContent = "Features";
+links[4].textContent = "About";
+links[5].textContent = "Contact";
